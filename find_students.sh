92f40af29671850741936f1b25c88fdfc8165731
Jackson Horan <jacksonhoran@Jacksons-MacBook-Pro.local>
2024-04-26 14:54:36 -0500

    initial Commit

---
+++ find_students.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#checks if the command given contains an id
+if [$# -eq 1]; then
+    echo "Please provide a student ID"
+    exit 
+fi
+
+# define student id variable and load the JSON file
+id=$1
+students=$(cat students.json)
+
+# iterates over the json data and defines both id and grade variable,
+# then takes the given student id and prints their grade
+found=false
+for i in $(echo "${students}" | jq -c '.[]'); do
+    id=$(echo "${i}" | jq -r '.id')
+    grade=$(echo "${i}" | jq -r '.grade')
+
+    if [ "${id}" -eq "${student_id}" ]; then
+        found=true
+        echo "Student ID: ${id}"
+        echo "Final Grade: ${grade}%"
+        break
+    fi
+done
+
+#displays error message incase the student is not found
+if ['$found' = false]; then
+    echo "Error. Student not found in data base"
+fi
